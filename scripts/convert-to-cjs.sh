b35adf9c4ea752a741c8e32f1fe13cdc457214b3
Matt Holtzman <matt.holtzman@gmail.com>
2023-10-13 11:38:53 -0400

    add safety check to script

--- scripts/convert-to-cjs.sh
+++ scripts/convert-to-cjs.sh
@@ -3,7 +3,14 @@
 # manually updates all of the emitted .js files to .cjs (including require statements)
 # so that the output is compatible with CommonJS
 
-find ./dist/cjs -type f -exec sh -c '
-  sed -r -i "s/require\((.+)\.js/require\(\1.cjs/g" $0
-  mv -- "$0" "${0%.js}.cjs"
-' {} \;
+OUTDIR=$1
+
+if [[ $OUTDIR == ./dist* ]]; then
+  find $OUTDIR -type f -exec sh -c '
+    sed -r -i "s/require\((.+)\.js/require\(\1.cjs/g" $0
+    mv -- "$0" "${0%.js}.cjs"
+  ' {} \;
+else
+  echo "Invalid output directory: $OUTDIR"
+  exit 1
+fi
